e12bcb71e5547ddd00e8658262e3eb49e41f02ac
RolandoDEBIAN <rolisrivas@outlook.com>
2024-03-12 11:46:20 -0600

    Hecho ejercicio 12 proj euler, falta optimizarlo

--- projectEuler/12_divisible_triangular_number.c
+++ projectEuler/12_divisible_triangular_number.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-int cant_divisores(int num)
+int cant_div(int num)
 {
-	int cont = 0;
-	for (int i = 12; i <= sqrt(num); ++i)
+	int factors = 1, i = 2, total = 1;
+	float raiz = sqrt(num);
+
+	while (i <= raiz)
 	{
-		if (num % i == 0) cont++;
+		while (num % i == 0)
+		{
+			num /= i;
+			++factors;
+		}
+		total *= factors;
+		factors = 1;
+		++i;
 	}
-	return cont;
+
+	return total;	
 }
 
 int main()
 {
-	int i = 0;
-	while (++i)
-	{
-		if (cant_divisores(i*(i+1) / 2) > 500)
-		{
-			printf("%d\n", i*(i+1) / 2);
-			break;
-		}
-	}
+	int i = 1, triangular_num;
+	
+	do {
+		triangular_num = i*(i+1) / 2;
+		++i;
+	} while ( cant_div(triangular_num) <= 500 );
+
+	printf("%d\n", triangular_num);
 }
 
