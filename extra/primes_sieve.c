23b8293525e5a7a18a1885a5810ff2a61cf39c1a
ChocoRolis <119354460+ChocoRolis@users.noreply.github.com>
2024-05-26 23:11:49 -0600

    Criba de eratostenes en C

---
+++ extra/primes_sieve.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int sieve(int size)
+{
+    int nums[size];
+    int root = sqrt(size);
+    int cont = 0;
+
+    memset(nums, 1, sizeof nums);
+
+    for (int i = 2; i <= root; ++i)
+    {
+        if (nums[i]) 
+        { 
+            for (int j = 0; (i*i + i*j) < size; ++j)
+            {
+                nums[i*i + i*j] = 0;
+            } 
+        }
+    }
+
+    for (int i = 2; i < size; ++i)
+    {
+        if (nums[i]) ++cont;
+    }
+        
+    return cont;
+}
+
+int main()
+{
+    int primes = sieve(1000000);
+    printf("%d\n", primes);
+}
